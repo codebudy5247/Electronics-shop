288841598122925363be931844fc3f5fac720120
codebudy5247 <a.shekhar5247@gmail.com>
2020-12-24 21:05:10 +0530

    Initial commit {Project setUp}

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+var colors = require("colors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyparser = require("body-parser");
+let app = express();
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+  () => console.log(`Database connected!`.green.bold)
+);
+
+//Middlewares
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+//Middleware for consoling every request
+app.use((req, res, next) => {
+  console.log(`${req.method}`.bold.green + `  ${req.originalUrl}`.dim);
+  next();
+});
+
+//Handlebars
+const exphbs = require("express-handlebars");
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "/views/",
+  })
+);
+app.set("view engine", "hbs");
+
+//App Listening {PORT}
+PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is Up and running on port ${PORT}`.yellow.bold);
+});
